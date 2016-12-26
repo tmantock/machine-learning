9178cfea8b41dd44f35047f169a4b471dd12f241
Tevin Mantock <tmantock@gmail.com>
2016-12-26 15:15:18 -0800

    move comments

--- multiple-linear-regression/multiple_linear_regression.py
+++ multiple-linear-regression/multiple_linear_regression.py
@@ -9,14 +9,6 @@ dataset = pd.read_csv('50_Startups.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
-#Feature Scaling
-"""
-from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)
-"""
-
 #Encoding categorical data
 #Encodeing the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -32,3 +24,11 @@ x = x[:, 1:]
 #Splitting the dataset into the training and test set
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state = 0)
+
+#Feature Scaling
+"""
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+"""
