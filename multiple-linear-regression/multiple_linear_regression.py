d4edac0a9cb9fb51c3c50ee108d4e82d671ecff3
Tevin Mantock <tmantock@gmail.com>
2016-12-26 15:15:00 -0800

    set up data pre processing fro multiple regression

---
+++ multiple-linear-regression/multiple_linear_regression.py
@@ -0,0 +1,34 @@
+#Multiple Linear Regression
+#Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+#Feature Scaling
+"""
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+"""
+
+#Encoding categorical data
+#Encodeing the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_X = LabelEncoder()
+x[:, 3] = labelencoder_X.fit_transform(x[:,3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+x = onehotencoder.fit_transform(x).toarray()
+
+#Avoiding the Dummy variable Trap
+x = x[:, 1:]
+
+#Splitting the dataset into the training and test set
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state = 0)
