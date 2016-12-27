a83d28f8c3c434c1ff72098ca1b1f63463ccb629
Tevin Mantock <tmantock@gmail.com>
2016-12-26 16:13:17 -0800

    determing the most significant ind variable

--- multiple-linear-regression/multiple_linear_regression.py
+++ multiple-linear-regression/multiple_linear_regression.py
@@ -42,6 +42,33 @@ regressor.fit(x_train, y_train)
 #Predicting the Test Set Results
 y_pred = regressor.predict(x_test)
 
-print y_pred
-print "================"
-print y_test
+#Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+
+#add 1 as a constatnt to the dataset for multiple linear regression
+x = np.append(arr = np.ones((50,1)).astype(int), values = x, axis = 1)
+#Create optimal matrix of features
+x_opt = x[:, [0, 1, 2, 3, 4, 5]]
+#import the the OLS class from statsmodel
+#Fit the full model with all possible predictors
+regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+#Get the summary report from the OLS model and get the p-values
+#The significance level will be SL = 0.05 or 5%
+regressor_OLS.summary()
+#Remove the variable with highest p-value and repeat the process when the hisghest p-value is found for backwards elimination
+x_opt = x[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regressor_OLS.summary()
+#Remove the variable with highest p-value and repeat the process when the hisghest p-value is found for backwards elimination
+x_opt = x[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regressor_OLS.summary()
+#Remove the variable with highest p-value and repeat the process when the hisghest p-value is found for backwards elimination
+x_opt = x[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regressor_OLS.summary()
+#Remove the variable with highest p-value and repeat the process when the hisghest p-value is found for backwards elimination
+x_opt = x[:, [0, 3]]
+regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regressor_OLS.summary()
+#R&D Spend is the most significant independent variable and is a good statistical predictor for profit
